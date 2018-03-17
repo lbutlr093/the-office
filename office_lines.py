4f29ae86dcd7daf3acbf7a8d0e8847f1f5c7b422
lbutlr093 <lbutlr093@gmail.com>
2018-03-17 10:45:12 -0500

    speakers appearance, and clean lines of dialog

--- office_lines.py
+++ office_lines.py
@@ -1,17 +1,24 @@
+import re
 import csv
 from collections import defaultdict
 
-speaker_dict = {}
+speaker_dict = {}			#Speakers, and how many lines they each have
+speaker_appear = {}			#which season each speaker appears in
 with open('the_office_lines.csv', 'r') as lines_file:
 	office_reader = csv.reader(lines_file, delimiter = ',')
 	header = office_reader.next()
 	for row in office_reader:
-		character = row[5]
+		dialog = re.sub(r'[^\x00-\x7f]',r'', row[4]).replace('_', '')		#cleaning dialog
+		#dialog					#each line of dialog
+		character = row[5]		#the character that speaks each line of dialog
 		if character not in speaker_dict:
 			speaker_dict[character] = 1
+			speaker_appear[character] = row[1]
 		else:
 			speaker_dict[character] += 1
-## top 5 characters sorted
-sorted_speaker_dict = sorted(speaker_dict, key = speaker_dict.get, reverse = True)[:5]
+
+## top 25 characters sorted
+sorted_speaker_dict = sorted(speaker_dict, key = speaker_dict.get, reverse = True)[:25]
+print('Top 25 characters with the most lines of dialog')
 for item in sorted_speaker_dict:
 	print(str(item) + ': ' + str(speaker_dict[item]))
